e4123c04f12fb2eaaf2c3ecb3b2239de39cb327f
Anton <antla@ciklum.com>
2022-12-15 22:41:42 +0200

    feat: add react-router-dom; add icons;  add nav links

--- frontend/src/components/AppHeader/AppHeader.jsx
+++ frontend/src/components/AppHeader/AppHeader.jsx
@@ -1,17 +1,20 @@
-import { Layout, Menu,Row } from 'antd'
+import { HomeTwoTone, ShoppingTwoTone, ThunderboltTwoTone } from '@ant-design/icons'
+import { Col,Layout, Row } from 'antd'
+import { Link } from 'react-router-dom'
+
 const { Header } = Layout
 
 
-export const AppHeader = () => (<Header>
-  <Row>
-    <Menu
-      theme='dark'
-      mode='horizontal'
-      defaultSelectedKeys={ ['2'] }
-      items={ new Array(3).fill(null).map((_, index) => ({
-        key: String(index + 1),
-        label: `nav ${index + 1}`,
-      })) }
-    />
-  </Row>
-</Header>)
+export const AppHeader = () => {
+  return (<Header>
+    <Row>
+      <Col span={ 18 }>
+        <Link to='/'><HomeTwoTone />E-Shop</Link>
+      </Col>
+      <Col span={ 6 }>
+        <Link to='/cart'><ShoppingTwoTone />Cart</Link>
+        <Link to='/login'><ThunderboltTwoTone />Sign-in</Link>
+      </Col>
+    </Row>
+  </Header>)
+}
