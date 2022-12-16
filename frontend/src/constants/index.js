76a49a0ed2817128d04241bf32729b363a2faa65
Anton <antla@ciklum.com>
2022-12-16 17:17:35 +0200

    feat: add mwdia query lib; add responsive design for Home screen

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,7 @@
+export const mediaQueryies = {
+  largedesktopOrLaptop: '(min-width: 1224px)',
+  smallDesktopOrLaptop: '(min-width: 900px) and (max-width: 1223px)',
+  tablet: '(min-width: 768px) and (max-width: 900px)',
+  mobile: '(max-width: 767px)'
+}
+export const NO_DATA = 'No data'
