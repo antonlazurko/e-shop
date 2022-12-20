5d19d092cf9ad3f16c00ecc586b542ef8787d9e5
Anton <antla@ciklum.com>
2022-12-20 17:31:04 +0200

    feat: add custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -14,21 +14,24 @@ router.get(
     asyncHandler(async(req, res) => {
     const products = await Product.find({})
     res.json(products)
-}))
+    })
+)
 
 // @desc Fetch single product
 // @route GET /api/products/:id
 // @access Public
 
-router.get('/:id', asyncHandler(async(req, res) => {
+router.get(
+    '/:id',
+    asyncHandler(async(req, res) => {
         const product = await Product.findById(req.params.id)
         if (product) {
             res.json(product)
         } else {
-            res.status(404).json({
-                message: 'Product not found'
-            })
+            res.status(404)
+            throw new Error('Product not found')
         }
-}))
+    })
+)
 
 export default router
