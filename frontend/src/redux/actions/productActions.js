4c8fa97767aa4f823f564b7a4fc5f6e13a425db6
Anton <antla@ciklum.com>
2022-12-20 23:12:34 +0200

    feat: bringing redux state into HomeScreen

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -10,7 +10,7 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({ type: PRODUCT_LIST_FAIL, payload:
          error.response && error.response.data.message ?
-           error.response.data.message :
-           error.data.message })
+           error.data.message :
+           error.message })
   }
 }
