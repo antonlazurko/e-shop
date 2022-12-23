d5d73e63ba2a7154475c1c8f61bb1a25db1b5216
Anton <antla@ciklum.com>
2022-12-23 18:42:10 +0200

    feat: add custom authentication middlware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,6 +21,26 @@ const authUser = asyncHandler(async(req, res) => {
         throw Error('Invalid credentials')
     }
 })
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Public
+const getUserProfile = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id)
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(401)
+        throw new Error('User not found!')
+    }
+    res.send('SUCCESS')
+})
 export {
-    authUser
+    authUser,
+    getUserProfile
 }
