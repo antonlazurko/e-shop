aa29e54da759dff37025275885d683f25956351e
Anton <antla@ciklum.com>
2022-12-23 22:48:16 +0200

    feat: add user registration and password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,9 +38,39 @@ const getUserProfile = asyncHandler(async(req, res) => {
         res.status(401)
         throw new Error('User not found!')
     }
-    res.send('SUCCESS')
 })
+
+// @desc Registar new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async(req, res) => {
+    const { name, email, password } = req.body
+    const userExist = await User.findOne({ email })
+    if (userExist) {
+        res.status(400)
+        throw new Error('User already exists!')
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else{
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 export {
     authUser,
-    getUserProfile
+    getUserProfile,
+    registerUser
 }
