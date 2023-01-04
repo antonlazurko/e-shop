372aa2366b822f37ca3b395e923f3823fc606c4e
Anton <antla@ciklum.com>
2023-01-04 16:08:52 +0200

    feat: add image upload config and endpiont

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,6 +10,4 @@ router.route('/').get(getProducts).post(protect, admin, createProduct)
 
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin,updateProduct)
 
-
-
 export default router
