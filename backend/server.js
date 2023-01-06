6262bdd578cb7950584a5ab274c21b40d8f96b28
Anton <antla@ciklum.com>
2023-01-06 22:12:35 +0200

    feat: add heroku config

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,6 @@ app.use(express.json({ limit: 10000 }))
 
 app.use('/api/', apiLimiter)
 
-app.get('/', (req, res) => {
-    res.send('API is running..')})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -50,6 +47,15 @@ app.use('/api/upload', uploadRoutes)
 app.use('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 const __dirname = path.resolve()
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running.....')
+    })
+}
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
